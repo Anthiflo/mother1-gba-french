fbe7b1b9f009d16481dd13d7058f713870e02bf8
JumpmanFR <jumpman@jumpman.fr>
2022-01-16 00:54:18 +0100

    Added check overlap for data inserted into the ROM

--- project/i.bat
+++ project/i.bat
@@ -1,6 +1,8 @@
 copy m12.gba test.gba
 @python converttext.py
 @introconv.exe
+@python check_overlap.py m12.asm
 @echo  Inserting new code
 @xkas test.gba m12.asm
 @insert.exe
+@del m1_main_text.txt
